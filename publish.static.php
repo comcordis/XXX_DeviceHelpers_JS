18c5839a293e28b38c8e5d18376513a0b121966e
vinceMeens <v.w.a.meens@comcordis.com>
2013-09-11 01:25:45 +0200

    Static publishing per project

---
+++ publish.static.php
@@ -0,0 +1,12 @@
+<?php
+
+XXX_Static_Publisher::publishMergedFilesFromOtherProject($project, $deployIdentifier, array
+(
+	'XXX_Device_Keyboard.js',
+	'XXX_Device_Mouse.js',
+	'XXX_Device_Screen.js',
+	'XXX_Device_TouchSurface.js'
+),
+'initialize.js');
+
+?>
